31b1f6b3aa349f94da6b88565ed7d1cccf456489
nosknut <nosknut@gmail.com>
2021-11-18 16:47:45 +0100

    Adds sensors endpoint to rest-api

--- public/pi-color-picker-pi-rest-api.py
+++ public/pi-color-picker-pi-rest-api.py
@@ -9,10 +9,11 @@
 # You should now see your canvas displayed on the pi
 #   in realtime after drawing new pixels
 
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from sense_hat import SenseHat
 from flask_cors import CORS, cross_origin
 sense = SenseHat()
+sense.set_imu_config(True, True, True)
 
 app = Flask(__name__)
 cors = CORS(app)
@@ -28,3 +29,37 @@ def result():
         for x, color in row.items():
             sense.set_pixel(int(x), int(y), color)
     return 'Ok!'
+
+
+@app.route('/sensors', methods=['GET'])
+@cross_origin()
+def get_sensor_data():
+    return jsonify({
+        'environmental': {
+            'temperature': {
+                'temperature': sense.get_temperature(),
+                'humidity': sense.get_temperature_from_humidity(),
+                'pressure': sense.get_temperature_from_pressure(),
+            },
+            'humidity': sense.get_humidity(),
+            'pressure': sense.get_pressure(),
+        },
+        'imu': {
+            'orientation': {
+                'radians': sense.get_orientation_radians(),
+                'degrees': sense.get_orientation_degrees(),
+            },
+            'compass': {
+                'compass': sense.get_compass(),
+                'raw': sense.get_compass_raw(),
+            },
+            'gyroscope': {
+                'gyroscope': sense.get_gyroscope(),
+                'raw': sense.get_gyroscope_raw(),
+            },
+            'accelerometer': {
+                'accelerometer': sense.get_accelerometer(),
+                'raw': sense.get_accelerometer_raw(),
+            },
+        },
+    })
