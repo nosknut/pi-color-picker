fb27f660922729c2636d5af3bdfd83c21e5e50d1
nosknut <nosknut@gmail.com>
2021-11-18 16:48:12 +0100

    Fixes color on nav icon in app bar

--- src/components/PageNavigation.tsx
+++ src/components/PageNavigation.tsx
@@ -19,8 +19,8 @@ export function PageNavigation() {
                 <Typography variant="h6">PI Color Picker</Typography>
                 <Box mr="auto" />
                 <Routes>
-                    <Route path="/" element={<IconButton component={Link} to="/sensors"> <Sensors /> </IconButton>} />
-                    <Route path="/sensors" element={<IconButton component={Link} to="/"> <Palette /> </IconButton>} />
+                    <Route path="/" element={<IconButton sx={{ color: "white" }} component={Link} to="/sensors"> <Sensors /> </IconButton>} />
+                    <Route path="/sensors/*" element={<IconButton sx={{ color: "white" }} component={Link} to="/"> <Palette /> </IconButton>} />
                 </Routes>
                 <ColorPickerTutorialButton />
                 <ThemeButton />
